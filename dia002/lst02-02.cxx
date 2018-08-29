91de848d8094f86bcf55a098884f173b9a07d5e6
ricardo-rios <ricardo.sv@gmail.com>
2018-08-29 13:41:46 -0600

    Add lst02-02.cxx

---
+++ dia002/lst02-02.cxx
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "Saludos a todos.\n"; 
+   cout << "Aquí hay un 5: " << 5 << "\n"; 
+   cout << "El manipulador endl escribe una nueva linea" <<
+           "en la pantalla.";  
+   cout << endl; 
+   
+   cout << "Aqui hay un numero muy grande:\t" << 70000 << endl; 
+   cout << "Aqui esta la suma de 8 y 5:\t" << 8+5 << endl; 
+   cout << "Aqui hay una fraccion:\t\t" << (float) 5/8 << endl;
+   cout << "Y un numero muy grande:\t"; 
+   cout << (double) 7000 * 7000 << 
+         endl;
+   cout << "!Ricardo Rios es un programador de C++!\n"; 
+   
+   return 0; 
+    
+    
+}
